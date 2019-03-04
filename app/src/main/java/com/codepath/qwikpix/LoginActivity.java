0e891d3e414221af7828a8e036a30a59086eac30
Kyle Smith <fenderpearl72@gmail.com>
2019-03-03 19:16:48 -0800

    Implemented new user option, logout option, and UI improvements

--- app/src/main/java/com/codepath/qwikpix/LoginActivity.java
+++ app/src/main/java/com/codepath/qwikpix/LoginActivity.java
@@ -1,34 +1,48 @@
 package com.codepath.qwikpix;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.app.FragmentTransaction;
 
 import com.parse.LogInCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
+import org.w3c.dom.Text;
+
+//parse-dashboard --appId qwik-pix --masterKey KeyForFastPix --serverURL "https://qwik-pix.herokuapp.com/parse"
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
-
+    private Context context;
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        context = this;
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,8 +51,71 @@ public class LoginActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Signup clicked");
+                showSignUp(context);
+            }
+        });
     }
 
+    private void showSignUp(Context context){
+        final Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+
+        final EditText etEmail = dialog.findViewById(R.id.etEmail);
+        final EditText etUsername = dialog.findViewById(R.id.etUsername);
+        final EditText etPassword = dialog.findViewById(R.id.etPassword);
+        final Button btnSignUp = dialog.findViewById(R.id.btnSignUp);
+        final Button btnCancel = dialog.findViewById(R.id.btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create the Parse user and set core properties
+                ParseUser user = new ParseUser();
+                user.setEmail(etEmail.getText().toString());
+                user.setUsername(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            //Success
+                            Toast.makeText(getApplicationContext(), "User Profile created!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            e.printStackTrace();
+                        }
+                        dialog.dismiss();
+
+                    }
+                });
+
+            }
+        });
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int dialogWidth = (int)(displayMetrics.widthPixels * 0.90);
+        int dialogHeight = (int)(displayMetrics.heightPixels * 0.90);
+        dialog.getWindow().setLayout(dialogWidth, dialogHeight);
+
+        dialog.show();
+
+    }
     private void login(String username, String password){
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
--- app/src/main/java/com/codepath/qwikpix/MainActivity.java
+++ app/src/main/java/com/codepath/qwikpix/MainActivity.java
@@ -7,9 +7,12 @@ import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.FileProvider;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,19 +33,26 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
+
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+
+    private File photoFile;
     private EditText etDescription;
     private Button btnCapture;
     private Button btnSubmit;
     private ImageView ivPostPic;
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public String photoFileName = "photo.jpg";
-    private File photoFile;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private DrawerLayout mDrawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //toolbar = findViewById(R.id.toolbar);
         etDescription = findViewById(R.id.etDescription);
         btnCapture = findViewById(R.id.btnCapture);
         ivPostPic = findViewById(R.id.ivPostPic);
@@ -71,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                 savePost(description, user, photoFile);
             }
         });
+
 
     }
 
